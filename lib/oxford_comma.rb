fe1a30bfdbd4da86ef46f6f5a0a8f169931db5fe
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-20 23:53:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
-  if array.length == 1 
+  if array.length == 1
   array.join('')
-  
-  elsif array.length == 2 
+
+  elsif array.length == 2
     array.join(' and ')
-    elsif array.length >= 3 
+    elsif array.length >= 3
       array[-1] = "and #{array[-1]}"
       array.join(', ')
-    else 
-      nil 
-    end 
+    else
+      nil
+    end
 end
